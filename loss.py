8299bc684ffa12dc60bf76734adedc9800c73bd0
Ramisa-Haq <ramisa.haque@stud.tu-darmstadt.de>
2025-11-18 14:13:01 +0100

    Update loss.py
    loss function fixed

--- loss.py
+++ loss.py
@@ -4,8 +4,7 @@ import klax
 
 class MSE(klax.Loss):
     """Mean Squared Error loss for batched model training."""
-
-    def __call__(self, model, batch):
+    def __call__(self, model, batch, batch_axis=None):
         x, y = batch
         y_pred = jax.vmap(model)(x)
         return jnp.mean(jnp.square(y - y_pred))
